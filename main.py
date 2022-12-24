6588b4f43de6ecf61ebcb361843a8e40fd68aa91
Rk_botz <118276768+Rishi09090909@users.noreply.github.com>
2022-12-24 19:08:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -134,10 +134,6 @@ async def message_handler(event):
             author=Config.BOT_USERNAME
         )
         message = f'**Click Here 👇 For "{event.text}"**\n\n[🍿🎬 {str(event.text).upper()}\n🍿🎬 {str("Click me for results").upper()}]({tgraph_result})'
-        
-        newbutton = [Button.url('🍿update channel 🍿',
-                                    f'https://t.me/rk_update')]
-        
         await txt.delete()
         result = await event.reply(message, link_preview=False)
         await asyncio.sleep(Config.AUTO_DELETE_TIME)
