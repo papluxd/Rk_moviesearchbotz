69d6c93a8f1cfbc7670f174dbb5ed08e531e2e3c
Rk_botz <118276768+Rishi09090909@users.noreply.github.com>
2022-12-24 16:37:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -113,7 +113,8 @@ async def message_handler(event):
             newbutton = [Button.url('Click To Check Spelling ✅',
                                     f'http://www.google.com/search?q={event.text.replace(" ", "%20")}%20Movie')], [
                             Button.url('Click To Check Release Date 📅',
-                                    f'http://www.google.com/search?q={event.text.replace(" ", "%20")}%20Movie%20Release%20Date')]
+                                    f'http://www.google.com/search?q={event.text.replace(" ", "%20")}%20Movie%20Release%20Date')],[Button.url('🍿Request to admin🍿',
+                                    f'https://t.me/RequestyourMovieherebot')]
             await txt.delete()
             result = await event.reply(answer, buttons=newbutton, link_preview=False)
             await asyncio.sleep(Config.AUTO_DELETE_TIME)
