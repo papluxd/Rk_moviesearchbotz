385bbc481eba6fc5de49770435a6acbf2f24f886
CrezyDeveloper <122361875+CrezyDeveloper@users.noreply.github.com>
2023-03-22 00:31:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Greymattersbot/Link-Search-Bot-Pro /MdiskWalaBot
-cd /MdiskWalaBot
+git clone https://github.com/CrezyDeveloper/Rk_moviesearchbotz /Rk_moviesearchbotz
+cd /Rk_moviesearchbotz
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
